f0904dd4a317fbc826cdc42f0745985a1f952a8d
LucasLira777 <lira.fire.2007@gmail.com>
2024-10-14 13:20:23 -0300

    metodo start e gerar grid

--- My project/Assets/Scripts/Grid.cs
+++ My project/Assets/Scripts/Grid.cs
@@ -10,4 +10,26 @@ public class Grid : MonoBehaviour
     public Material headMaterial;
     public Material fruitMaterial;
     private Renderer[] gameBlocks;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        gameBlocks = new Renderer[areaResolution * areaResolution];
+        for (int x = 0; x < areaResolution; x++)
+        {
+            for (int y = 0; y < areaResolution; y++)
+            {
+                GameObject quadPrimitive = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                quadPrimitive.transform.position = new Vector3(x, 0, y);
+                Destroy(quadPrimitive.GetComponent<Collider>());
+                quadPrimitive.transform.localEulerAngles = new Vector3(90, 0, 0);
+                quadPrimitive.transform.SetParent(transform);
+                gameBlocks[(x * areaResolution) + y] = quadPrimitive.GetComponent<Renderer>();
+            }
+        }
+    }
 }
