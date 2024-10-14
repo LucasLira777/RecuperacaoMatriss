1272219dff5b99ee50e793f321613cf2b209b19e
LucasLira777 <lira.fire.2007@gmail.com>
2024-10-14 10:15:26 -0300

    metodo InicializeSnake
    criação do metodo InicializeSnake

--- My project/Assets/Scripts/MovimentoCobra.cs
+++ My project/Assets/Scripts/MovimentoCobra.cs
@@ -10,4 +10,15 @@ public class MovimentoCobra : MonoBehaviour
     private Direction snakeDirection = Direction.Right;
     private int fruitBlockIndex = -1;
     private int totalPoints = 0;
+
+    public void InitializeSnake()
+    {
+        snakeCoordinates.Clear();
+        int startCoordinate = Random.Range(0, gridManager.areaResolution - 1) + (gridManager.areaResolution * 3);
+        snakeCoordinates.Add(startCoordinate);
+        snakeCoordinates.Add(startCoordinate - gridManager.areaResolution);
+        snakeCoordinates.Add(startCoordinate - (gridManager.areaResolution * 2));
+        fruitBlockIndex = -1;
+        snakeDirection = Direction.Right; // Inicialmente, a cobra vai para a direita
+    }
 }
